d114e5e540b7f7a0b758f69c7a682f379516d763
@cookie <emir2104@gmail.com>
2021-05-31 00:36:07 -0500

    auth firebase prueba

--- src/App.js
+++ src/App.js
@@ -1,17 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+import firebase from "firebase";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bienvenido a mi página web personal 😎❤🤗
-        </p>
-      </header>
-    </div>
-  );
+firebase.initializeApp({
+  apiKey: "AIzaSyD6CtMwABa5H2WvAVToslx7R-eTHDkMeok",
+  authDomain: "fir-auth-tutorial-c2599.firebaseapp.com"
+})
+
+class App extends Component {
+  state = { isSignedIn: false };
+
+  uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    ],
+    callbacks: {
+      signInSuccess: () => false,
+    },
+  };
+
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged((user) => {
+      this.setState({ isSignedIn: !!user });
+      console.log("user", user);
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.isSignedIn ? (
+          <span>
+            <div>REGISTRATE POR FAVOR!</div>
+            <button onClick={() => firebase.auth().signOut()}>CERRAR CESION!</button>
+            <h1>BIENVENIDO TILIN(A): {firebase.auth().currentUser.displayName}</h1>
+            <img
+              alt="profile picture"
+              src={firebase.auth().currentUser.photoURL}
+            />
+          </span>
+        ) : (
+          <StyledFirebaseAuth
+            uiConfig={this.uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
+//git run deploy
